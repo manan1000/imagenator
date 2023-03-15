0e06611f23fe04c6ccf0edbadfbbea237dca6f73
manan29 <105338607+Manan-Hemani@users.noreply.github.com>
2023-03-15 19:40:22 +0530

    added more random prompts

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
         if(form.prompt){
             try{
                 setGeneratingImg(true);
-                const response = await fetch("http://localhost:8000/api/v1/dalle" , {
+                const response = await fetch("https://imagenator-backend.onrender.com/api/v1/dalle" , {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
@@ -48,7 +48,7 @@ const CreatePost = () => {
             setLoading(true);
 
             try{
-                const response = await fetch("http://localhost:8000/api/v1/post" ,{
+                const response = await fetch("https://imagenator-backend.onrender.com/api/v1/post" ,{
                     method: "POST",
                     headers: {
                         'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     const fetchPost = async () => {
         setLoading(true);
         try{
-            const response = await fetch("http://localhost:8000/api/v1/post", {
+            const response = await fetch("https://imagenator-backend.onrender.com/api/v1/post", {
                 method: "GET",
                 headers: {
                     'Content-Type': 'application/json',
